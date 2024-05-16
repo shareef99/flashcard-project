60217ce1ce7e3cd3a344b83ed3ca3913862a547f
Nadeem Shareef <nadeemshareef934@gmail.com>
2024-05-16 18:33:04 +0300

    setup: tailwindcss

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
@@ -8,15 +8,6 @@ export const Route = createRootRoute({
 function Root() {
   return (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -7,7 +7,7 @@ export const Route = createLazyFileRoute("/")({
 function Index() {
   return (
     <div className="p-2">
-      <h1>Welcome Home!</h1>
+      <h1 className="text-red-600">Welcome Home!</h1>
     </div>
   );
 }
